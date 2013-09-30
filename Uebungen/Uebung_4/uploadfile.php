16ff6d3faa57b948ab87df57575dabfda988c97c
Rajeevan Rabeendran <rajeevan@bluewin.ch>
2013-09-30 18:56:51 +0200

    Aufgabe.1

---
+++ Uebungen/Uebung_4/uploadfile.php
@@ -0,0 +1,20 @@
+<?php 
+// In PHP kleiner als 4.1.0 sollten Sie $HTTP_POST_FILES anstatt 
+// $_FILES verwenden.
+
+$uploaddir = '/var/www/uploads/';
+$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+
+echo '<pre>';
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+    echo "Datei ist valide und wurde erfolgreich hochgeladen.\n";
+} else {
+    echo "Möglicherweise eine Dateiupload-Attacke!\n";
+}
+
+echo 'Weitere Debugging Informationen:';
+print_r($_FILES);
+
+print "</pre>";
+
+?>
